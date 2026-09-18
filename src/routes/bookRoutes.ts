808aa09860d8133269b6ca78f878671b5e1c816a
sT10072303 <mrphineas1104@gmail.com>
2026-09-18 13:51:44 +0200

    Adding GET book by id on bookrouts

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -32,8 +32,19 @@ router.post("/", (req, res) => {
     const newBook: Book = {id: books.length + 1, title, year, authorId};
 
     books.push(newBook);
-
     res.status(201).json(newBook);
+});
+
+// GET /books/:id
+// Returns a single book by ID
+router.get("/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const book = books.find((book) => book.id === id);
+
+    if (!book) {
+        return res.status(404).json({message: "Book not found"});
+    }
+    res.status(200).json(book);
 });
 
 export default router;
