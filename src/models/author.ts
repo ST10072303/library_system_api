038fc0d1408ff0fbba4b0a0bb23960c0f1c73df2
sT10072303 <mrphineas1104@gmail.com>
2026-09-18 13:27:25 +0200

    Adding Book model and bookroutes.ts

--- src/models/author.ts
+++ src/models/author.ts
@@ -1,6 +1,4 @@
-// Author model
 // This interface defines the structure of an author.
-
 export interface Author {
     id: number;
     name: string;
---
+++ src/models/book.ts
@@ -0,0 +1,7 @@
+// This interface defines the structure of a book.
+export interface Book {
+    id: number;
+    title: string;
+    year: number;
+    authorId: number;
+}
